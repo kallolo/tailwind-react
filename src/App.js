c9368266d9f0131ca5b38ddcfc3a0f13529892aa
Mustajib <mustajib.sif@gmail.com>
2020-09-26 15:15:38 +0700

    konfigurasi tailwindCSS pada react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="bg-gray-500">
+    <header>
+      <img src={logo} className="w-1/4" alt="logo" />
+      <p className="bg-blue-300 text-black">
+        Tailwind CSS
+      </p>
+    </header>
+  </div>
+
   );
 }
 
